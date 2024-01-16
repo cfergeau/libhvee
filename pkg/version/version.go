474cf77593f03692e4e31439ee492d3b94660d42
Christophe Fergeau <cfergeau@redhat.com>
2024-01-16 13:40:10 +0100

    version: Add versioning for github tarballs
    When building from github tarballs, getting the version from `git
    describe` won't work, or worse, will be irrelevant [1]
    However, we can make use of $Format$ and .gitattributes as described in
    [2] to automatically substitute the correct version in a go variable
    when `git archive` is used.
    In particular, the correct version number will automatically be
    substituted when GitHub creates release tarballs.
    
    [1] this can happen if the tarball is built through rpmbuild with
    `%autosetup -Sgit -n %{name}-%{version}`, which unpacks
    the release tarball in a newly created git repository.
    
    [2] https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
    
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
+	"strings"
 )
 
-// set using the '-X github.com/containers/libhvee/pkg/version.gitVersion' linker flag
-var gitVersion string = ""
+var (
+	// set using the '-X github.com/containers/libhvee/pkg/version.gitVersion' linker flag
+	gitVersion string = ""
+	// set through .gitattributes when `git archive` is used
+	// see https://icinga.com/blog/2022/05/25/embedding-git-commit-information-in-go-binaries/
+	gitArchiveVersion = "$Format:%(describe)$"
+)
 
 func ModuleVersion() string {
 	return fmt.Sprintf("%s version %s", os.Args[0], moduleVersion())
@@ -15,6 +21,9 @@ func ModuleVersion() string {
 
 func moduleVersion() string {
 	switch {
+	// This will be substituted when building from a GitHub tarball
+	case !strings.HasPrefix(gitArchiveVersion, "$Format:"):
+		return gitArchiveVersion
 	// This will be set when building from git using make
 	case gitVersion != "":
 		return gitVersion
