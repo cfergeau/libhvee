b8698838366d31742998abd10a7d9d1ba8fec3be
Christophe Fergeau <cfergeau@redhat.com>
2024-01-16 13:37:26 +0100

    version: Add moduleVersionFromBuildInfo
    This adds correct version information for `kvpctl version` when
    it's installed using:
    ```
    go install github.com/containers/libhvee/cmd/kvpctl
    ```
    
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,7 @@ package version
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 )
 
 // set using the '-X github.com/containers/libhvee/pkg/version.gitVersion' linker flag
@@ -13,6 +14,23 @@ func ModuleVersion() string {
 }
 
 func moduleVersion() string {
+	switch {
 	// This will be set when building from git using make
-	return gitVersion
+	case gitVersion != "":
+		return gitVersion
+	// moduleVersionFromBuildInfo() will be set when using `go install`
+	default:
+		return moduleVersionFromBuildInfo()
+	}
+}
+
+func moduleVersionFromBuildInfo() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	if info.Main.Version == "(devel)" {
+		return ""
+	}
+	return info.Main.Version
 }
